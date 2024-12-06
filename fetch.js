dfef1a3d8fe68f26f76ae256d8704211c2fc6ceb
Michael Kohler <me@michaelkohler.info>
2024-12-06 23:09:56 +0100

    Add Wiki test script

---
+++ fetch.js
@@ -0,0 +1,5 @@
+import fetch from 'node-fetch';
+
+const URL = "https://wiki.mozilla.org/api.php?action=query&list=recentchanges&rcprop=title|timestamp|user&rclimit=500&format=json"
+
+fetch(URL, {}).then((res) => res.json()).then((json) => console.log("Result", json.query.recentchanges))
