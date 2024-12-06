21270b1b43602c28810dd09fcaedc6028a275e52
Michael Kohler <me@michaelkohler.info>
2024-12-06 23:17:52 +0100

    Send custom UA

--- fetch.js
+++ fetch.js
@@ -1,3 +1,9 @@
 const URL = "https://wiki.mozilla.org/api.php?action=query&list=recentchanges&rcprop=title|timestamp|user&rclimit=500&format=json"
 
-fetch(URL, {}).then((res) => res.json()).then((json) => console.log("Result", json.query.recentchanges))
+fetch(URL, {
+  headers: {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:135.0) Gecko/20100101 Firefox/135.0',
+  }
+})
+  .then((res) => res.json())
+  .then((json) => console.log("Result", json.query.recentchanges))
