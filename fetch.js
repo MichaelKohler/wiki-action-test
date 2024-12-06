d50d8b46c280461f38a3546300f85b0479263e0a
Michael Kohler <me@michaelkohler.info>
2024-12-06 23:11:35 +0100

    Use internal fetch

--- fetch.js
+++ fetch.js
@@ -1,5 +1,3 @@
-import fetch from 'node-fetch';
-
 const URL = "https://wiki.mozilla.org/api.php?action=query&list=recentchanges&rcprop=title|timestamp|user&rclimit=500&format=json"
 
 fetch(URL, {}).then((res) => res.json()).then((json) => console.log("Result", json.query.recentchanges))
